b6f74b1d7e5e49cbd310c44c8b2f022782b028e5
Sol <1921403065@qq.com>
2022-03-26 19:24:04 +0800

    hot

---
+++ src/test/java/com/atguigu/git/GutTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+public class GutTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git12");
+        System.out.println("hot");
+
+
+    }
+}
